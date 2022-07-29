6e0013ebd7bb6581f1b745aac2c760ed59db7688
Alexander <valverin95@gmail.com>
2022-07-30 02:31:44 +0300

    feat: added categories table

--- db/db.go
+++ db/db.go
@@ -15,9 +15,31 @@ func Init() {
 		panic("failed to connect database")
 	}
 
-	if err = db.AutoMigrate(&models.User{}); err != nil {
+	if err = db.AutoMigrate(
+		&models.User{},
+		&models.Category{},
+	); err != nil {
 		log.Fatal("error while auto migrate running", err)
 	}
 
 	DB = db
+
+	initCategories()
+}
+
+func initCategories() {
+	categories := []models.Category{
+		{ID: 1, Name: "food"},
+		{ID: 2, Name: "transport"},
+		{ID: 3, Name: "home"},
+		{ID: 4, Name: "fan"},
+		{ID: 5, Name: "health"},
+		{ID: 6, Name: "rent"},
+		{ID: 7, Name: "subscription"},
+		{ID: 8, Name: "restaurant"},
+	}
+
+	for _, cat := range categories {
+		DB.FirstOrCreate(&cat)
+	}
 }
