567c1bf34819f1f8cf8773ab1f56d9547f8f4578
Alexander <valverin95@gmail.com>
2022-07-30 03:20:44 +0300

    feat: [DB.User] updated json config

--- db/models/User.go
+++ db/models/User.go
@@ -6,14 +6,14 @@ import (
 )
 
 type User struct {
-	ID        uint `gorm:"primaryKey"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 
-	Login     string
-	Email     string `gorm:"not null"`
-	Firstname string
-	Surname   string
-	Password  string
+	Login     string `json:"login"`
+	Email     string `gorm:"not null" json:"email"`
+	Firstname string `json:"firstname"`
+	Surname   string `json:"surname"`
+	Password  string `json:"-"`
 }
