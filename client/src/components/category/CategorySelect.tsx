076617d2b46a91e1061be85e5a03a69a7f96a8c0
Alexander <valverin95@gmail.com>
2022-08-09 15:58:38 +0300

    ✨ [ui] Components: CategorySelect

---
+++ client/src/components/category/CategorySelect.tsx
@@ -0,0 +1,60 @@
+import { createSignal, For, onMount, ParentProps } from 'solid-js';
+import { Option, Select, SelectProps } from '@solsy/ui';
+import { categoryApi } from '@app/services/api';
+import { Category } from '@app/services/mappers';
+
+type Props = {
+  id: number;
+  onInput?: (category: Category) => void;
+} & SelectProps;
+
+export const CategorySelect = (props: ParentProps<Props>) => {
+  const [catsMap, setMap] = createSignal<Map<number, Category>>(new Map());
+  const [cats, setCats] = createSignal<Category[]>([]);
+
+  onMount(async () => {
+    const cats = await categoryApi.getAll();
+    /**
+     * Todo: исправить селект в solsy.ui
+     * при получения значения типа 0 | '' - не отрабатывает кастомное отображение;
+     * необходимо всегда отбражать, кроме явного значения null | undefined
+     */
+    setMap(cats);
+    setCats([...cats.values()]);
+  });
+
+  const customView = (id: number | string) => {
+    const cat = catsMap().get(Number(id));
+    return (
+      <span class="flex items-center gap-2">
+        <i class={cat?.icon}/>
+        {cat?.name}
+      </span>
+    );
+  };
+
+  const onInput = (id: number | string) => {
+    const cat = catsMap().get(Number(id));
+    if (cat) {
+      props.onInput?.(cat);
+    }
+  };
+
+  return (
+    <Select
+      {...props}
+      value={props.id}
+      customValue={customView}
+      onInput={onInput}
+    >
+      <For each={cats()}>
+        {cat => (
+          <Option value={cat.id + ''}>
+            <i class={cat.icon}/>
+            {cat.name}
+          </Option>
+        )}
+      </For>
+    </Select>
+  );
+};
