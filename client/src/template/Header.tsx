2c67e466a0be1cb3bd34193b0dda3e5df61724cf
Alexander <valverin95@gmail.com>
2022-08-10 02:02:46 +0300

    ✨ [FE] Header: auth menu

--- client/src/template/Header.tsx
+++ client/src/template/Header.tsx
@@ -1,8 +1,10 @@
 import { Component, createEffect, createSignal, Show } from 'solid-js';
 import { Link } from 'solid-app-router';
 import { DrawerState, useApp } from '@app/providers';
+import { Button, Popover } from '@solsy/ui';
 
 export const Header: Component = () => {
+  const [popover, setPopover] = createSignal(false);
   const [ref, setRef] = createSignal<HTMLElement>();
   const app = useApp();
 
@@ -35,6 +37,41 @@ export const Header: Component = () => {
       </div>
 
       <div class="flex-none">
+        <Show when={app.state.user}>
+          <Popover
+            show={popover()}
+            openByTriggerClick={false}
+            trigger={
+              <Button color="ghost" onClick={() => setPopover(!popover())}>
+                <i class="fa-solid fa-user"/>
+              </Button>
+            }
+          >
+            <ul
+              class="menu bg-base-300 rounded-box"
+              onClick={() => setPopover(false)}
+            >
+              <li>
+                <Link href="/profile">
+                  <div class="flex items-center gap-2">
+                    <i class="fa-solid fa-envelope"/>
+                    {app.state.user.email}
+                  </div>
+                </Link>
+              </li>
+
+              <li>
+                <Link href="/auth">
+                  <div class="flex items-center gap-2">
+                    <i class="fa-solid fa-arrow-right-from-bracket"/>
+                    Log out
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          </Popover>
+        </Show>
+
         <Show when={!app.state.user}>
           <Link href="/auth" class="btn btn-ghost btn-sm normal-case text-sm">
             Login
